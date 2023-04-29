95ff289539843fa91fc130af1ede932e03d82606
tengxong <119726458+tengxong@users.noreply.github.com>
2023-04-29 16:47:03 +0700

    what6

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import Index from '../router'
+import Index from '../router/index'
 import './index.css'
 
 
