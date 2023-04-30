8ac4c3bc0787b9ea1dfe023c8e8c9ac0b8212834
tengxong <119726458+tengxong@users.noreply.github.com>
2023-04-30 14:30:24 +0700

    what15

--- src/router/ContactUs.jsx
+++ src/router/ContactUs.jsx
@@ -7,7 +7,7 @@ export default function ContactUs() {
       <Typography variant='h4' align='center'>Login</Typography>
       <Box display='flex' flexDirection={{xs:'column',md:'row',height:'100vh'}}justifyContent={'center'} alignItems={'center'}>
         <Box width={'50%'} display='flex' justifyContent={'center'} alignItems={'centers'}>
-          <img src='../src/Image/Email-Transparent-PNG.webp' width={'60%'} />
+          <img src='./src/Image/Email-Transparent-PNG.webp' width={'60%'} />
         </Box>
         <Stack width={{md:'40%',xs:'100%'}} spacing={2} justifyContent={'center'} alignItems={'center'}>
           <TextField id="filled-basic" label="Firstname" sx={{width:{md:'70%' , xs:'100%'}}}/>
